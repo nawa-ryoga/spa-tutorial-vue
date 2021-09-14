ab1040560093195a21a5932a3d09826ad62a1f67
nawawa <oonawa12@gmail.com>
2021-09-14 14:34:02 +0900

    タスクリストコンポーネント追加

--- server/spa_tutorial/src/router/index.js
+++ server/spa_tutorial/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import TopComponent from '@/components/TopComponent'
+import TaskListComponent from '@/components/TaskListComponent'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'top-component',
       component: TopComponent
+    },
+    {
+      path: '/tasks',
+      name: 'task.list',
+      component: TaskListComponent
     }
   ]
 })
